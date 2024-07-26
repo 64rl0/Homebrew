4101c49a7ecfe4dbeaaa29cd28466935a1476233
carlogtt <carlogtt@amazon.co.uk>
2024-07-26 11:47:39 +0100

    carlogtt updates

---
+++ homebrew.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+FORMULAS=(
+	'bash'
+	'bat'
+	'git'
+	'vim'
+	'jesseduffield/lazygit/lazygit'
+	'shc'
+	'shellcheck'
+	'starship'
+	'terminal-notifier'
+	'tmux'
+	'unison'
+	'zsh-autosuggestions'
+	'zsh-syntax-highlighting'
+	'fzf'
+	'wget'
+	'awscli'
+	'python@3.12'
+	'python@3.11'
+	'python@3.10'
+	'python@3.9'
+)
+
+CASKS=(
+    'alacritty'
+	'gitkraken'
+	'postman'
+	'sublime-text'
+	'rectangle-pro'
+	'vlc'
+	'notion'
+	'wireshark'
+	'jetbrains-toolbox'
+	'maccy'
+	'transmit'
+)
+
+for formula in "${FORMULAS[@]}"; do
+    echo brew install $formula
+done
+
+
+for cask in "${CASKS[@]}"; do
+    echo brew install --cask $cask
+done
+
